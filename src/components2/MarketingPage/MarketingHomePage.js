fa2c815d31a93cce8f9ead6ee2bbdd58e727e72d
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-08-14 20:18:17 -0700

    building second design header

---
+++ src/components2/MarketingPage/MarketingHomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Header from "./Header";
+
+const MarketingHomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
+
+export default MarketingHomePage;
