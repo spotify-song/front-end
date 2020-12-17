de47257fc5dbe727f18669132cfe632b14c678e0
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-17 13:47:34 -0800

    feat(content/footer): add content and footer component
    implemented compnents for content and footer from the design schema, added marketing imgs as well

---
+++ src/componentsv2/marketingPage/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+   return (
+      <>
+         <footer className="Footer">
+            <div className="inner-footer">
+               <h1>then share your</h1>
+               <h1>top ten chart</h1>
+               <button>start today</button>
+            </div>
+         </footer>
+         <div className="feedback">
+            <p>let us know what you think of the Topsounds.me website. <a href="https://example.com">Leave feedback</a></p>
+         </div>
+      </>
+   )
+}
+
+export default Footer;
